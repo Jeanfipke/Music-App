7ef8284afa235ea03f6216c72524a89d3d7faf4e
Jean <jeanfipke@gmail.com>
2023-01-07 17:40:46 -0300

    Search input and button done

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -3,7 +3,9 @@ import React, { Component } from 'react';
 class NotFound extends Component {
   render() {
     return (
-      <div data-testid="page-not-found"></div>
+      <div data-testid="page-not-found">
+        <p>Esta página não existe 😥</p>
+      </div>
     );
   }
 }
--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,10 +2,40 @@ import React, { Component } from 'react';
 import Header from '../components/Header';
 
 class Search extends Component {
+  state = {
+    isButtonDisabled: true,
+    // isLoading: false,
+  };
+
+  isValue = ({ target: { value } }) => {
+    const dois = 2;
+    if (value.length >= dois) {
+      this.setState({ isButtonDisabled: false });
+    } else {
+      this.setState({ isButtonDisabled: true });
+    }
+  };
+
   render() {
+    const { isButtonDisabled } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
+        <form>
+          <input
+            placeholder="digite o nome de um artista ou banda"
+            type="text"
+            data-testid="search-artist-input"
+            onChange={ this.isValue }
+          />
+          <button
+            data-testid="search-artist-button"
+            type="button"
+            disabled={ isButtonDisabled }
+          >
+            Pesquisar
+          </button>
+        </form>
       </div>
     );
   }
